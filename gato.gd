f58fc1962103b3e66c8403986b675c95f6bef126
Miguel Angel Cano Mesa <miguelangelcanomesa@gmail.com>
2023-11-18 01:24:09 +0100

    gatete moviendose

--- gato.gd
+++ gato.gd
@@ -1,11 +1,11 @@
-extends Node2D
+extends RigidBody2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#$gato.add_constant_central_force(Vector2(-10,0))
-	#$gato.set_axis_velocity(Vector2(-100,0))
-	pass
+	$AnimatedSprite2D.play('idle')
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$gato.set_axis_velocity(Vector2(-100,0))
+	pass
