470bea8e18deeace1b75a7306767f260dbe16d7e
Miguel Angel Cano Mesa <miguelangelcanomesa@gmail.com>
2023-11-18 20:30:47 +0100

    estable

--- main.gd
+++ main.gd
@@ -28,23 +28,31 @@ func _input(event):
 # Obtén las coordenadas del evento de clic en relación con esta escena
 			var click_position = event.position
 # Capturamos la ventana al hacer click
-			var imagen = get_viewport().get_texture()
-			var image_data = imagen.get_data()
-			var image_path = "res://screenshot.png"
+	# Obtiene la imagen de la pantalla
+			var screenshot = get_viewport().get_texture().get_image()
+
+			# Guarda la imagen como un archivo PNG (puedes ajustar la ruta y el nombre del archivo)
+			var file = FileAccess.open("user://screenshot.png", FileAccess.WRITE)
+
+			screenshot.save_png("user://screenshot.png")
 			
-			var image = Image.new()
+			file.close()
+			#var imagen = get_viewport().get_texture()
+			#var image_data = imagen.get_image()
 			
-			image.create_from_data(imagen.get_width(), imagen.get_height(), false, Image.FORMAT_RGBA8, image_data)
+			#var image = Image.new()
+			
+			#image.create_from_data(imagen.get_width(), imagen.get_height(), false, Image.FORMAT_RGBA8, image_data)
 			
 		# Guardar la imagen como archivo PNG (puedes ajustar la ruta y el nombre del archivo)
 			#if not FileAccess.file_exists("user://savegame.save"):
 			#	FileAccess.open("user://polaroid.save", FileAccess.WRITE)
 				
-			var file = FileAccess.open("user://polaroid.save", FileAccess.WRITE)
+			#var file = FileAccess.open("user://polaroid.save", FileAccess.WRITE)
 			
 			#file.open("user://polaroid.save", FileAccess.WRITE)
-			image.save_png(file)
-			file.close()
+			#image_data.save_png(file)
+			#file.close()
 			
 			await get_tree().create_timer(0.1).timeout
 			if get_node("gato").velocidad == 0:
