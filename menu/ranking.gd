2a32650db2b07b59f1c75773f145a97d3fcb2845
Miguel Angel Cano Mesa <miguelangelcanomesa@gmail.com>
2023-11-19 17:37:00 +0100

    ranking

--- menu/ranking.gd
+++ menu/ranking.gd
@@ -15,12 +15,29 @@ func _ready():
 	var json = JSON.new()
 		
 	var datos = json.parse(json_string)
-		
+	
 	$Jugador1_name.text = json.get_data()["1"]["nombre"]
-	# Acceder a los datos
-	#var nombre = datos["nombre"]
-	#var edad = datos["edad"]
-	#var trabajo = datos["trabajo"]
+	$Jugador2_name.text = json.get_data()["2"]["nombre"]
+	$Jugador3_name.text = json.get_data()["3"]["nombre"]
+	$Jugador4_name.text = json.get_data()["4"]["nombre"]
+	$Jugador5_name.text = json.get_data()["5"]["nombre"]
+	$Jugador6_name.text = json.get_data()["6"]["nombre"]
+	$Jugador7_name.text = json.get_data()["7"]["nombre"]
+	$Jugador8_name.text = json.get_data()["8"]["nombre"]
+	$Jugador9_name.text = json.get_data()["9"]["nombre"]
+	$Jugador10_name.text = json.get_data()["10"]["nombre"]
+	
+	$jugador1_puntos.text = str(json.get_data()["1"]["value"])
+	$jugador2_puntos.text = str(json.get_data()["2"]["value"])
+	$jugador3_puntos.text = str(json.get_data()["3"]["value"])
+	$jugador4_puntos.text = str(json.get_data()["4"]["value"])
+	$jugador5_puntos.text = str(json.get_data()["5"]["value"])
+	$jugador6_puntos.text = str(json.get_data()["6"]["value"])
+	$jugador7_puntos.text = str(json.get_data()["7"]["value"])
+	$jugador8_puntos.text = str(json.get_data()["8"]["value"])
+	$jugador9_puntos.text = str(json.get_data()["9"]["value"])
+	$jugador10_puntos.text = str(json.get_data()["10"]["value"])
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
